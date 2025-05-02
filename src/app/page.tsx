dbe2a203aefeae0733045c0af3d3484ac61a2980
KaaronZ <geri0525@gmail.com>
2025-05-03 00:18:55 +0200

    API works yay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,23 @@ import SearchResults from "./searchResults/searchresults"
 export default function Home() {
   const [summonerName, setSummonerName] = useState(''); 
   const [summonerTag, setSummonerTag] = useState('');
+  const [puuid, setPuuid] = useState('');
+
+
 
   const handleSearch = async (summonerName: string, summonerTag: string) => {
     setSummonerName(summonerName); 
     setSummonerTag(summonerTag);
     const response = await fetch(`http://localhost:3000/api/getSummonerData?summoner=${summonerName}&tag=${summonerTag}`)
     const data = await response.json();
-    console.log("I am data coming from page.tsx", data);
+    console.log(data);
+    setPuuid(data.puuid);
   };
+  const showData = async () => {
+    console.log("I am puuid", puuid);
+    console.log("I am summonerName", summonerName);
+    console.log("I am summonerTag", summonerTag);
+  }
 
 
   return (
@@ -21,7 +30,8 @@ export default function Home() {
       <h1 className="text-4xl font-bold text-center mt-8">League Profile Showcase</h1>
       <p className="text-center mt-4">Show off your League of Legends profile with style.</p>
       <SearchBar onSearch={handleSearch} />
-      <SearchResults/>
+      <SearchResults puuid={puuid} />
+      <button onClick={showData}>FOR LOGGING</button>
     </div>
   );
 }
--- src/app/searchResults/searchresults.tsx
+++ src/app/searchResults/searchresults.tsx
@@ -1,20 +1,25 @@
 'use client'
 
+type SearchResultsProps = {
+    puuid: string;
+}
 
-
-export default function SearchResults(){
+export default function SearchResults({puuid}: SearchResultsProps){
 
     const handleMatchFetch = async ()=>{
         const response = await fetch(`http://localhost:3000/api/getMatchData?puuid=${puuid}`)
         const data = await response.json();
         console.log(data)
+        console.log("I am puuid", puuid)
     }
 
     return(
         <div>
             <button
             onClick={handleMatchFetch}
-            ></button>
+            >
+                I AM SEARCH RESULTS
+            </button>
         </div>
     )
 }
